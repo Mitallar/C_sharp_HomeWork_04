842dc85354c19f0013157dec414d7929ac2416b6
Sheveliow Igor <strelanedura@list.ru>
2023-09-01 12:45:34 +0300

    Added hometasks

---
+++ hometask_3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов,
+// заполненных псевдослучайными числами и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+void WriteInArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(41);
+    }
+}
+
+void DerivationArr(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+
+int[] array = new int [8];
+
+WriteInArray(array);
+DerivationArr(array);
